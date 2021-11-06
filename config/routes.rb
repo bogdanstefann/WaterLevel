57ef94819d8523bc083b0659dc53599ae4965d8d
bogdanstefann <bogdanstefann@gmail.com>
2021-11-06 20:44:35 +0200

    Added the admin column in the user instead of a separate record. Improved data visibility

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admins
   devise_for :users
-
   root 'septic_tank_levels#index'
+
   resources :septic_tank_levels
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_06_093238) do
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
+ActiveRecord::Schema.define(version: 2021_11_06_183447) do
 
   create_table "septic_tank_levels", force: :cascade do |t|
     t.float "level"
@@ -39,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_11_06_093238) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
